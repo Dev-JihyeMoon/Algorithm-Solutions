b27db6f90437635f75f1b986a77893b5aa994f6b
Jihye Moon <jihyer0395@gmail.com>
2025-08-25 18:41:13 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(bf.readLine());
+		
+		for(int i=0; i<n; i++) {
+			for(int j=i+1; j<n; j++) {
+				bw.write(" ");
+			}
+			for(int j=0; j<i+1; j++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		
+		bw.close();
+	}
+}
