e7685b400abba60055e6fcdbde54c5a1a7fdc013
Jihye Moon <jihyer0395@gmail.com>
2025-08-26 13:56:24 +0900

    [Silver III] Title: N과 M (7), Time: 660 ms, Memory: 162180 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+	
+	static int n, m;
+	static int[] arr;
+	static int[] output;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		
+		n = input[0];
+		m = input[1];
+		arr = new int[n];
+		output = new int[m];
+		
+		arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		Arrays.sort(arr);
+		
+		func(0);
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+	public static void func(int count) throws IOException {
+		if(count == m) {
+			for(int i=0; i<m; i++) {
+				bw.write(output[i]+" ");
+			}
+			bw.write("\n");
+			return;
+		}
+		
+		for(int i=0; i<n; i++) {
+			output[count] = arr[i];
+			func(count+1);
+		}
+	}
+}
