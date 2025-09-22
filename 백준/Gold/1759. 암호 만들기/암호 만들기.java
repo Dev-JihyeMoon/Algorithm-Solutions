3386f0a5bbf6fc4c7f981a555160a07a18bc9055
Jihye Moon <jihyer0395@gmail.com>
2025-09-22 17:52:01 +0900

    [Gold V] Title: 암호 만들기, Time: 136 ms, Memory: 14796 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -4,49 +4,56 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-	static String[] alp, word, vowel = {"a", "e", "i", "o", "u"};
-	static boolean[] isVisited;
+	
+	static String[] alp, word;
+	static boolean[] isVisited, vowel = new boolean[26];
 	static int L, C;
 	
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		
 		L = input[0];
 		C = input[1];
 		alp = br.readLine().split(" ");
 		
 		Arrays.sort(alp);
 		
+		//모음 확인을 위한 세팅
+		char[] temp = {'a', 'e', 'i', 'o', 'u'};
+		for(char i:temp) {
+			vowel[(int)i-97] = true;
+		}
+		
 		word = new String[L];
 		isVisited = new boolean[C];
+		
 		getPassword(0, 0);
 		
 		br.close();
 	}
 	
+	//구성된 암호 안에 모음, 자음이 조건을 만족하는지 확인
 	public static boolean getvalidCheck() {
-		int vowelCount=0; 
+		int count=0; 
+		
 		for(String i:word) {
-			for(String j:vowel) {
-				if(i.equals(j)) vowelCount++;
-			}
+			if(vowel[i.charAt(0) - 'a']) count++;
 		}
 		
-		if(vowelCount==0) return false;
+		if(count==0) return false;
 		
-		return 2<=L-vowelCount?true:false;
+		return 2<=L-count?true:false;
 	}
 	
-	public static void getPassword(int cnt, int front) {
-		
-		//System.out.println(String.valueOf(cnt));
-		
+	public static void getPassword(int cnt, int front) {		
 		if(cnt == L) {
-			//구성된 암호 안에 모음, 자음이 조건을 만족하는지 확인
-			if(getvalidCheck()) {
-				for(int i=0; i<L; i++) {
-					System.out.print(word[i]);
+			
+			//모음 자음 조건을 충족한 경우 출력
+			if(getvalidCheck()) { 
+				for(String i:word) {
+					System.out.print(i);
 				}
 				System.out.println();
 			}
