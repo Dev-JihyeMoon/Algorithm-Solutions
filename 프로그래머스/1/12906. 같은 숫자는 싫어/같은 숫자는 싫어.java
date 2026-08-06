7dfe3ac3c5e77c88880c108a5944da420503e3b5
Jihye Moon <jihyer0395@gmail.com>
2026-08-06 20:13:17 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 49.52 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+        
+        for(int i:arr){
+            if(stack.peek() != i){
+                stack.push(i);
+            }
+        }
+        
+        int[] answer = new int[stack.size()];
+        for(int i=stack.size()-1; i>=0; i--){
+            answer[i] = stack.pop();
+        }
+        
+        
+        return answer;
+    }
+}
