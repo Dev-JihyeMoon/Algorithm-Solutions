7994db64638896986bd5a7a8c2704ee6870434e1
Jihye Moon <jihyer0395@gmail.com>
2026-08-16 12:45:16 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.52 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = String.valueOf(s.charAt(s.length()/2));
+        
+        if(s.length()%2 == 0){
+            answer = String.valueOf(s.charAt((s.length()/2)-1)) + answer;
+        }
+        return answer;
+    }
+}
